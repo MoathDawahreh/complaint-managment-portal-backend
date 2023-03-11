7e999d6541218891d1d81624209ae5967d108735
MoathDawahreh <moath.dawahreh@gmail.com>
2023-03-12 00:30:00 +0300

    new module and gurds

---
+++ src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUser(userId: number) {
+    // middleware to verify the user is authorized to access this api? // check if is admin
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) return null;
+
+    delete user.pwd;
+    return user;
+  }
+}
